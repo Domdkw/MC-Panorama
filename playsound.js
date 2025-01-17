e69327b1f24e41f66787da0c3ff78f4f482f2d67
Domdkw <166027502+Domdkw@users.noreply.github.com>
2025-01-17 17:28:33 +0800

    优化点击

--- playsound.js
+++ playsound.js
@@ -11,7 +11,6 @@ const randomIndex = Math.floor(Math.random() * audioFiles.length);
 const selectedAudioFile = audioFiles[randomIndex];
 audio.src = selectedAudioFile;
 
-// 定义一个变量来保存当前滑块的音量值
 let currentVolume = parseFloat(volumeSlider.value);
 
 // 定义一个变量来保存点击音效的 Audio 对象
@@ -31,15 +30,16 @@ volumeSlider.addEventListener('input', function () {
 });
 
 document.body.addEventListener('click', function (Click_ogg) {
-  if (Click_ogg.target.tagName === 'BUTTON') {
-    if (currentVolume > 0) { // 只有在音量大于 0 时才处理点击音效
+  const Click_ogg_button = Click_ogg.target.closest('button'); // 查找最近的按钮元素
+  if (Click_ogg_button) {
+    if (currentVolume > 0) {
       if (!clickAudio) {
         // 如果 clickAudio 还没有创建，则创建一个新的 Audio 对象
         clickAudio = new Audio('Click.ogg.mp3');
       }
       clickAudio.volume = currentVolume;
       if (clickAudio.paused) {
-        clickAudio.currentTime = 0; // 重置播放时间到开头
+        clickAudio.currentTime = 0;
         clickAudio.play();
       }
     }
